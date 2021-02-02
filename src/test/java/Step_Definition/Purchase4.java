3f388e1395e79d59e6ca9e8f4c40df2d7928bb4f
EGBETEOK <egbeteok@gmail.com>
2021-02-02 15:25:20 +0200

    Purchase4 Test

---
+++ src/test/java/Step_Definition/Purchase4.java
@@ -0,0 +1,80 @@
+package Step_Definition;
+
+import Pages.Purchase4Page;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class Purchase4 {
+    public WebDriver driver;
+
+    @Given("^I am on the home$")
+    public void iAmOnTheHome() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.navigate().to("http://automationpractice.com/");
+        driver.manage().window().maximize();
+
+    }
+
+    @When("^I place an order$")
+    public void iPlaceAnOrder() throws InterruptedException {
+
+        Purchase4Page page = PageFactory.initElements(driver, Purchase4Page.class);
+
+        Actions builder = new Actions(driver);
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[4]/div/div[1]/div/a[1]/img"));
+        builder.moveToElement(element).build().perform();
+        //This code clicks on the "more" button after you have hovered your mouse on the product you want
+        driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[4]/div/div[2]/div[2]/a[2]/span")).click();
+        driver.findElement(By.id("quantity_wanted")).clear();
+        //This line changes clears the quanity field
+        driver.findElement(By.id("quantity_wanted")).sendKeys("3");
+        //This line Changes the size
+        Select size =new Select(driver.findElement(By.id("group_1")));
+        size.selectByVisibleText("L");
+        //page.typeQuantity();
+        //page.selectSize();
+        //page.clickColour();
+
+        page.clickAddToCart();
+        Thread.sleep(3000);
+        page.clickProceedToCheckOut();
+        page.clickProceed();
+        page.typeEmailAddress();
+        page.typePassword();
+        page.clickLogin();
+        page.clickSubmitOrder();
+        page.clickCheckBox();
+        page.clickSubmitAgain();
+
+    }
+
+
+    @And("^I make payment$")
+    public void iMakePayment() {
+
+        Purchase4Page page = PageFactory.initElements(driver, Purchase4Page.class);
+        page.clickPay();
+        page.clickConfirm();
+
+
+
+    }
+
+    @Then("^I should successfully complete my order$")
+    public void iShouldSuccessfullyCompleteMyOrder() {
+
+        Purchase4Page page = PageFactory.initElements(driver, Purchase4Page.class);
+        page.assertOrderSuccess();
+
+    }
+}
